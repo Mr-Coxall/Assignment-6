da59d778b3d847c76782fefc34d899536af71448
Patrick Coxall <mr.coxall@mths.ca>
2017-11-10 15:07:54 -0500

    Add in code to show built in Pythonista cards

---
+++ game_21.py
@@ -0,0 +1,37 @@
+# Created by: Mr. Coxall
+# Created on: Sep 2016
+# Created for: ICS3U
+# This program plays 21, using classes
+
+import ui
+
+# global varibles
+user_card1_image = None
+user_card2_image = None
+user_card3_image = None
+dealer_card1_image = None
+dealer_card2_image = None
+dealer_card3_image = None
+
+def show_cards(sender):
+    # show cards
+    
+    user_card1_image = ui.Image.named('card:BackBlue2')
+    view['user_card1_imageview'].image = user_card1_image
+    user_card2_image = ui.Image.named('card:BackBlue2')
+    view['user_card2_imageview'].image = user_card2_image
+    user_card3_image = ui.Image.named('card:BackBlue2')
+    view['user_card3_imageview'].image = user_card3_image
+    view['user_card3_imageview'].alpha = 0.1
+    dealer_card1_image = ui.Image.named('card:BackBlue2')
+    view['dealer_card1_imageview'].image = user_card1_image
+    dealer_card2_image = ui.Image.named('card:BackBlue2')
+    view['dealer_card2_imageview'].image = user_card2_image
+    dealer_card3_image = ui.Image.named('card:BackBlue2')
+    view['dealer_card3_imageview'].image = user_card3_image
+    
+    
+view = ui.load_view()
+view.present('fullscreen')
+
+show_cards(None)
